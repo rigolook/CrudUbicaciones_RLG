d2a7a4016d48cfd09378ffdc53e59c6611dd4cb9
rigo l.g <138252884+rigolook@users.noreply.github.com>
2023-09-30 08:22:54 -0700

    Update ubicaciones_BILL.cs

--- BLL/ubicaciones_BILL.cs
+++ BLL/ubicaciones_BILL.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class ubicaciones_BILL
+    public class ubicacionesBLL
     {
+        public int ID { get; set; }
+        public string Ubicacion { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
     }
 }
